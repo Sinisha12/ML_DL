c8f4232a8c0e6988274b20d8699bfbd334571360
Sinisha123 <p.sinisha12@gmail.com>
2020-04-27 13:57:15 +0200

    daily update + basic unit test

--- flask/flaskblog/posts/routes.py
+++ flask/flaskblog/posts/routes.py
@@ -3,6 +3,7 @@ from flaskblog import db
 from flaskblog.posts.forms import PostForm
 from flaskblog.model import Post
 from flask_login import current_user, login_required
+from flaskblog.users.utils import save_picture_post
 
 posts = Blueprint('posts', __name__)
 
@@ -12,7 +13,8 @@ posts = Blueprint('posts', __name__)
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        post = Post(title=form.title.data, content=form.content.data,
+                    author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post has been create!', 'success')
